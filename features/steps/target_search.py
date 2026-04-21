e3876444a8c653ade0381e4fb26264dce2613b70
Sisi Zhou <zhousisi1990@gmail.com>
2026-04-20 20:29:31 -0700

    Update product search test case and add Behave variables

--- features/steps/target_search.py
+++ features/steps/target_search.py
@@ -9,18 +9,12 @@ def open_target_main(context):
     sleep(2)
 
 
-@when("Search for tea")
-def search_product(context):
-    context.driver.find_element(By.ID, 'search').send_keys('tea')
+@when("Search for {search_query}")
+def search_product(context,search_query):
+    context.driver.find_element(By.ID, 'search').send_keys(search_query)
     context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
     sleep(7)
 
-
-@when("Search for coffee")
-def search_product(context):
-    context.driver.find_element(By.ID, 'search').send_keys('coffee')
-    context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
-    sleep(7)
 
 @when("Click on Cart icon")
 def click_cart_icon(context):
@@ -37,18 +31,12 @@ def click_sign_in_from_navigation(context):
      context.driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
      sleep(7)
 
-@then("Verify search results for tea shown")
-def verify_search_results(context):
-    expected_result = 'tea'
+@then("Verify search results for {product} shown")
+def verify_search_results(context,product):
     actual_result = context.driver.find_element(By.XPATH, "//div[contains(@class, 'styles_resultCount')]").text
-    assert expected_result in actual_result, f'Expected "{expected_result}" not in actual "{actual_result}"'
+    assert product in actual_result, f'Expected "{product}" not in actual "{actual_result}"'
 
 
-@then("Verify search results for coffee shown")
-def verify_search_results(context):
-    expected_result = 'coffee'
-    actual_result = context.driver.find_element(By.XPATH, "//div[contains(@class, 'styles_resultCount')]").text
-    assert expected_result in actual_result, f'Expected "{expected_result}" not in actual "{actual_result}"'
 
 @then("Verify “Your cart is empty” message is shown")
 def verify_cart_empty_msg(context):
